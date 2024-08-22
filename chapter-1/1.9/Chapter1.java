40286513b03bc359ebd861c67d22dd0ee1b81505
HamedAhmedHamed <hamedahmedgamed@gmail.com>
2024-08-22 14:57:13 +0300

    solution 1.9

---
+++ chapter-1/1.9/Chapter1.java
@@ -0,0 +1,10 @@
+public class Chapter1 {
+  public static void main(String[] args) {
+      final double width  = 4.5;
+      final double height = 7.9;
+
+      double area = width * height;
+
+      System.out.println("area: " + area);
+  } 
+}
