15ecaf95c3d5c8a126836a2f63e3dad4c5ec1a26
HamedAhmedHamed <hamedahmedgamed@gmail.com>
2024-08-22 15:18:28 +0300

    solution 1.11

---
+++ chapter-1/1.11/Chapter1.java
@@ -0,0 +1,20 @@
+public class Chapter1 {
+  public static void main(String[] args) {
+    final int    currentPopulation   = 312_032_486;
+    final double birthPerSeconds     = 1.0/7;
+    final double deathPerSeconds     = 1.0/13;
+    final double immigrantPerSeconds = 1.0/45;
+
+    final double second = 1;
+    final double minute = 60  * second;
+    final double hour   = 60  * minute;
+    final double day    = 24  * hour;
+    final double year   = 356 * day;
+
+    double changeInPopulation   = birthPerSeconds + immigrantPerSeconds - deathPerSeconds;
+    double newPopulation        = 5 * year * changeInPopulation;
+    double populationProjection = newPopulation + currentPopulation;
+
+    System.out.println("population projection: " + populationProjection);
+  } 
+}
